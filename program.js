ccb1d30c4b446384ea6a9469e11428fca34ce8c2
Krzysztof Palczewski <kpalcze@gmail.com>
2018-07-22 22:36:51 +0200

    ex4

--- program.js
+++ program.js
@@ -1,1 +1,9 @@
-console.log('HELLO WORLD'); 
+var fs = require('fs');
+
+fs.readFile(process.argv[2], function onFinishedReading(err, fileContents) {
+  if (err) {
+    return console.log(err);
+  }
+  var numOfLines = fileContents.toString().split('\n').length - 1;
+  console.log(numOfLines);
+});
