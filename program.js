35a32ffef17a7f38141cd5b539294a24c9277f18
Krzysztof Palczewski <kpalcze@gmail.com>
2018-07-22 22:23:21 +0200

    ex3

--- program.js
+++ program.js
@@ -1,1 +1,7 @@
-console.log('HELLO WORLD'); 
+var fs = require('fs');
+
+var buf = fs.readFileSync(process.argv[1]);
+buf = buf.toString();
+var numOfLines = buf.split('\n').length - 1;
+
+console.log(lines);
