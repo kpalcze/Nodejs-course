7e0b9fd86819d51a9a5e1aee7ad24b484b1c0dc1
Krzysztof Palczewski <kpalcze@gmail.com>
2018-07-22 23:20:32 +0200

    ex5

--- program.js
+++ program.js
@@ -1,1 +1,18 @@
-console.log('HELLO WORLD'); 
+const fs = require('fs');
+const path = require('path');
+const dirName = process.argv[2];
+const extention = "." + process.argv[3];
+
+fs.readdir(dirName, function onFinishedReading(err, listOfFiles) {
+    if (err)
+       return console.log(err);
+    displayFileNames(listOfFiles);
+});
+
+function displayFileNames(files) {
+    for (var i = 0; i < files.length; i++) {
+        if (path.extname(files[i]) === extention) {
+            console.log(files[i]);
+        }
+    }
+}
