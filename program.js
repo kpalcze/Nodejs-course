1302afe02d15ea27894cb5bd991d916bfd53f8e6
Krzysztof Palczewski <kpalcze@gmail.com>
2018-07-23 21:45:53 +0200

    ex8

--- program.js
+++ program.js
@@ -1,17 +1,33 @@
 const http = require('http');
 
-var fullResponse = "";
+var responses = [];
+var fetchedRespones = 0;
 
-http.get(process.argv[2], function callback(response) {
-    response.setEncoding('utf-8');
-    response.on('error', function (error) {
-       console.error(error); 
+for (var i = 2; i < 5; i++) {
+    getData(process.argv[i], i);
+}
+
+function getData(url, index) {
+    var fullResponse = "";
+    http.get(url, function callback(response) {
+        response.setEncoding('utf-8');
+        response.on('error', function (error) {
+           console.error(error); 
+        });
+        response.on('data', function (data) {
+            fullResponse += data;
+        });
+        response.on('end', function () {
+            responses[index - 2] = fullResponse;
+            fetchedRespones++;
+            if (fetchedRespones === 3)
+                displayResponses();
+        });
     });
-    response.on('data', function (data) {
-        fullResponse += data;
-    });
-    response.on('end', function () {
-        console.log(fullResponse.length);
-        console.log(fullResponse);
-    });
-});
+}
+
+function displayResponses() {
+    for (var i = 0; i < 3; i++) {
+        console.log(responses[i]);
+    }
+}
