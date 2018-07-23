c107924d716b3265979eb32a00fbfa8087e8d6a7
Krzysztof Palczewski <kpalcze@gmail.com>
2018-07-23 21:02:58 +0200

    ex7

--- program.js
+++ program.js
@@ -1,1 +1,11 @@
-console.log('HELLO WORLD'); 
+const http = require('http');
+
+http.get(process.argv[2], function callback (response) {
+    response.setEncoding('utf8')
+    response.on("error", function (error) {
+        console.error(error);
+    });
+    response.on("data", function (data) {
+        console.log(data);
+    }); 
+});
