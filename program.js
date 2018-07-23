89f7816ffdad51ecb4c9b61c7f4975e5ad833c46
Krzysztof Palczewski <kpalcze@gmail.com>
2018-07-23 22:50:19 +0200

    ex10

--- program.js
+++ program.js
@@ -1,1 +1,11 @@
-console.log('HELLO WORLD'); 
+const net = require('net');
+const moment = require('moment');
+
+var server = net.createServer(function listener(socket) {
+    socket.end(getCurrentDate());
+});
+server.listen(Number(process.argv[2]));
+
+function getCurrentDate() {
+    return moment().format('YYYY-MM-DD kk:mm') + '\n';
+}
