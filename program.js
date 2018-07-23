a64c3d19cbaa5428d964882afad734820739f18c
Krzysztof Palczewski <kpalcze@gmail.com>
2018-07-23 23:24:09 +0200

    ex11

--- program.js
+++ program.js
@@ -1,1 +1,9 @@
-console.log('HELLO WORLD'); 
+const http = require('http');
+const fs = require('fs');
+
+var server = http.createServer(function callback(request, response) {
+    response.writeHead(200, { 'content-type': 'text/plain' });
+    var fileStream = fs.createReadStream(process.argv[3]);
+    fileStream.pipe(response);
+});
+server.listen(Number(process.argv[2]));
