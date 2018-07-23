968ff17ef98fee59800ee79314602308c7ae5469
Krzysztof Palczewski <kpalcze@gmail.com>
2018-07-23 21:14:30 +0200

    ex8

--- program.js
+++ program.js
@@ -1,1 +1,17 @@
-console.log('HELLO WORLD'); 
+const http = require('http');
+
+var fullResponse = "";
+
+http.get(process.argv[2], function callback(response) {
+    response.setEncoding('utf-8');
+    response.on('error', function (error) {
+       console.error(error); 
+    });
+    response.on('data', function (data) {
+        fullResponse += data;
+    });
+    response.on('end', function () {
+        console.log(fullResponse.length);
+        console.log(fullResponse);
+    });
+});
