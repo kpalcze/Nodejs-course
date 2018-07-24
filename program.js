d77d0a73ac89419a372201b5c49d504c6fcd9aed
Krzysztof Palczewski <kpalcze@gmail.com>
2018-07-24 10:49:50 +0200

    ex12

--- program.js
+++ program.js
@@ -1,1 +1,12 @@
-console.log('HELLO WORLD'); 
+const http = require('http');
+const map = require('through2-map');
+
+var server = http.createServer(function (request, response) {
+    if (request.method === 'POST') {
+        request.pipe(map(function (chunk) {
+            return chunk.toString().toUpperCase();
+        })).pipe(response);
+    } 
+});
+    
+server.listen(Number(process.argv[2]));
